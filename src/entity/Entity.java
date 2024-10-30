045c69087eea827b88d6328d24d084e5f29f97b6
ac1 <ac1original.lol@gmail.com>
2024-10-30 21:49:04 +0100

    :zap: Refactor abstract class with generics for type-safe chaining in subclasses

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -3,20 +3,29 @@ import graphics.animation.Animation;
 import graphics.animation.AnimationManager;
 import main.GamePanel;
 import main.Main;
+import org.jetbrains.annotations.Nullable;
+import utils.Logger;
 
+import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 
-public abstract class Entity {
+@SuppressWarnings("ALL")
+public abstract class Entity<T extends Entity<?>> {
     public static final String DEFAULT_PATH = "/res/entity/";
     protected int x = 0, y = 0, width = 50, height = 50, speed = 10;
     protected Direction direction = Direction.RIGHT;
     private boolean moving = false;
     private Animation animation;
+    private final GamePanel gp;
+
+    public Entity(GamePanel gp) {
+        this.gp = gp;
+    }
 
     public abstract BufferedImage getImage();
-    public abstract void onTick();
-    public abstract void onAdd();
-    public abstract void onRemove();
+    public abstract T onTick();
+    public abstract T onAdd();
+    public abstract T onRemove();
 
     public int distanceTo(Entity entity) {
         return distanceTo(entity.x, entity.y);
@@ -26,25 +35,37 @@ public abstract class Entity {
         return Math.abs(this.x-x + this.y-y);
     }
 
-    public void playAnimation(Animation animation) {
+    public T playAnimation(Animation animation) {
+        if (getAnimationManager() == null) {
+            Logger.log(this.getClass(), "Failed playing animation " + animation.getClass().getSimpleName() + ". AnimationManager is null", true);
+            return (T) this;
+        }
         if (this.animation != null) {
             if (this.animation.getClass() != animation.getClass()) {
                 stopAnimation();
-                this.animation = getGamePanel().getAnimationManager().play(animation);
+                if (getGamePanel() != null) {
+                    this.animation = getAnimationManager().play(animation);
+                }
             }
         } else {
-            this.animation = getGamePanel().getAnimationManager().play(animation);
+            this.animation = getAnimationManager().play(animation);
         }
+        return (T) this;
     }
 
-    public void stopAnimation() {
+    public T stopAnimation() {
         if (animation != null) {
-            getGamePanel().getAnimationManager().stop(animation);
+            if (getAnimationManager() == null) {
+                Logger.log(this.getClass(), "Failed to stop animation " + animation.getClass().getSimpleName() + ". AnimationManager is null", true);
+                return (T) this;
+            }
+            getAnimationManager().stop(animation);
             animation = null;
         }
+        return (T) this;
     }
 
-    public void move(Direction direction) {
+    public T move(Direction direction) {
         setDirection(direction);
         switch (direction) {
             case UP -> y -= speed;
@@ -53,6 +74,7 @@ public abstract class Entity {
             case RIGHT -> x += speed;
         }
         setMoving(true);
+        return (T) this;
     }
 
     public int getX() {
@@ -74,24 +96,27 @@ public abstract class Entity {
     public int getSpeed() {
         return speed;
     }
-    public void setSpeed(int speed) {
+
+    public T setSpeed(int speed) {
         this.speed = speed;
+        return (T) this;
     }
 
     public Direction getDirection() {
         return direction;
     }
 
-    public void setDirection(Direction direction) {
+    public T setDirection(Direction direction) {
         this.direction = direction;
-    }
-
-    public AnimationManager getAnimationManager() {
-        return Main.getGamePanel().getAnimationManager();
+        return (T) this;
     }
 
     public final GamePanel getGamePanel() {
-        return Main.getGamePanel();
+        return gp == null ? Main.getGamePanel() : gp;
+    }
+
+    public AnimationManager getAnimationManager() {
+        return gp.getAnimationManager();
     }
 
     public Animation getAnimation() {
@@ -102,7 +127,8 @@ public abstract class Entity {
         return moving;
     }
 
-    public void setMoving(boolean moving) {
+    public T setMoving(boolean moving) {
         this.moving = moving;
+        return (T) this;
     }
 }
--- src/entity/player/Player.java
+++ src/entity/player/Player.java
@@ -1,5 +1,6 @@
 package entity.player;
 
+import entity.Direction;
 import entity.Entity;
 import graphics.ImageLoader;
 import graphics.animation.animations.player.PlayerIdleAnimation;
@@ -9,14 +10,28 @@ import user.UserKeyboardInput;
 
 import java.awt.image.BufferedImage;
 
-public class Player extends Entity {
+public class Player extends Entity<Player> {
     private final UserKeyboardInput userKeyboardInput = GamePanel.register(new UserKeyboardInput(this));
     private final BufferedImage fullImage = ImageLoader.getAutomated(DEFAULT_PATH + "player/player_idle_right.png", "player_idle_right");
     private BufferedImage playerImage = fullImage.getSubimage(0, 0, fullImage.getWidth()/12, fullImage.getHeight());
 
+    public Player(GamePanel gp) {
+        super(gp);
+    }
+
     @Override
     public BufferedImage getImage() {
         return playerImage;
+    }
+
+    @Override
+    public Player onTick() {
+        if (isMoving()) {
+            playAnimation(new PlayerWalkAnimation(this));
+        } else {
+            playAnimation(new PlayerIdleAnimation(this));
+        }
+        return this;
     }
 
     public void setImage(BufferedImage image) {
@@ -24,26 +39,34 @@ public class Player extends Entity {
     }
 
     @Override
-    public void onTick() {
-        if (isMoving()) {
-            playAnimation(new PlayerWalkAnimation(this));
-        } else {
-            playAnimation(new PlayerIdleAnimation(this));
-        }
-    }
-
-    @Override
-    public void onAdd() {
+    public Player onAdd() {
         width = 38*11;
         height = 28*11;
         speed = 5;
         playAnimation(new PlayerIdleAnimation(this));
+        return this;
     }
 
+
     @Override
-    public void onRemove() {}
+    public Player onRemove() {
+        return this;
+    }
 
     public UserKeyboardInput getUserKeyboardInput() {
         return userKeyboardInput;
+    }
+
+    public void keyPressed(char key) {
+        switch (key) {
+            case 'w' -> move(Direction.UP);
+            case 'a' -> move(Direction.LEFT);
+            case 's' -> move(Direction.DOWN);
+            case 'd' -> move(Direction.RIGHT);
+        }
+    }
+
+    public void keyReleased(char key) {
+        setMoving(false);
     }
 }
