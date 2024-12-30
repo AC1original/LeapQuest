d57250f83818280ad5ac5b00dfbac02968220650
AC1original <ac1original.lol@gmail.com>
2024-12-30 20:02:59 +0100

    :pencil2: Typooo

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -13,7 +13,7 @@ import java.awt.image.BufferedImage;
 
 //TODO: Better hitbox
 //TODO: Heath & enemies
-//TODO: Stop fall when bump head
+//TODO: Stop jump when bump head
 @SuppressWarnings({"UnusedReturnValue", "unchecked"})
 public abstract class Entity<T extends Entity<?>> {
     public static final String DEFAULT_PATH = "/res/entity/";
